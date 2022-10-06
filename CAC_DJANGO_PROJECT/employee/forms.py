a5a3cc69ccf2838cb6e291845e047965c00509ee
lumato92 <lucasmtorrado@gmail.com>
2022-10-06 20:39:05 -0300

    Correccion parcial forms empleados

--- CAC_DJANGO_PROJECT/employee/forms.py
+++ CAC_DJANGO_PROJECT/employee/forms.py
@@ -8,6 +8,8 @@ GENRE = (
         ('F','FEMENINO'),
         ('M','MASCULINO'),
     )
+
+
     
     
 
@@ -28,11 +30,11 @@ class EmployeeForm(forms.ModelForm):
         self.fields['tax_id_number'].widget = forms.NumberInput(attrs={'class':'form-control'})
         self.fields['address'].widget = forms.TextInput(attrs={'class':'form-control'})
         self.fields['phone'].widget = forms.NumberInput(attrs={'class':'form-control'})
-        self.fields['position'] = forms.ModelMultipleChoiceField(queryset=Puesto.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+        # self.fields['position'] = forms.ModelMultipleChoiceField(queryset=Puesto.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
         self.fields['start_date'].widget = forms.DateInput(attrs={'type':'date','class':'form-control'})
         self.fields['is_active'].widget = forms.CheckboxInput(attrs={'class':'form-check-input  justify-content-center'})
-        self.fields['management'] = forms.ModelMultipleChoiceField(queryset=Department.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
-        self.fields['manager'] = forms.ModelMultipleChoiceField(queryset=Employee.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+        # self.fields['management'] = forms.ModelMultipleChoiceField(queryset=Department.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
+        # self.fields['manager'] = forms.ModelMultipleChoiceField(queryset=Employee.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
         self.fields['salary'].widget = forms.NumberInput(attrs={'class':'form-control'})
 
     
