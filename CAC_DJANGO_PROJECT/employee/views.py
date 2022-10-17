10744f61dc3321e867b1490c78c5523e9a9a8f15
Chino <carlos.caserotto@yahoo.com>
2022-10-17 09:47:01 -0300

    Add delete to Todos los Empleados

--- CAC_DJANGO_PROJECT/employee/views.py
+++ CAC_DJANGO_PROJECT/employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from employee.models import Employee
@@ -6,19 +6,14 @@ from .forms import EmployeeForm
 # Create your views here.
 
 def index(request):
-    
     return HttpResponse("Pagina empleados")
     
 def addEmployee(request):
-    
     edit = False
-    
     if (request.method == 'POST'):
         form = EmployeeForm(request.POST)
         if form.is_valid():
-            
             form.save()
-        
             return HttpResponse("Empleado agregado")
         else:
             print(form.errors.as_data())
@@ -26,64 +21,45 @@ def addEmployee(request):
             # context = {'form':form}
             # return render(request,'employee/add2.html',context)
             return HttpResponse("HUBO UN ERROR")
-    
     else:       
         form = EmployeeForm()
-    
         context = {'form':form,
                    'edit': edit}
-    
     return render(request,'employee/addemployee.html',context)
 
 
 def allEmployees(request):
-    
     employees = Employee.objects.all()
-    
     context = { 'employees' : employees}    
     return render(request,'employee/allemployee.html', context)
 
-
 def infoEmployee(request,id):
     employee = Employee.objects.get(id=id)
-    
     context = {'employee' : employee}
-    
     return render(request,'employee/infoemployee.html',context)
-
-
-
-
 
 def editEmployee(request,id):
     employee = Employee.objects.get(id=id)
-    edit = True
-    
+    edit = True    
     if request.method == 'POST':
         form = EmployeeForm(request.POST,instance=employee)
         if form.is_valid():
-            
             form.save()
-        
             return HttpResponse("Empleado Editado")
         else:
             print(form.errors.as_data())
-
             return HttpResponse("HUBO UN ERROR")
-        
     else:
-    
-    
-        
         employeeform = EmployeeForm(instance = employee)
-    
         context = {'form' : employeeform,
                    'edit' : edit}
-    
     return render(request,'employee/addemployee.html',context)
 
+def deleteEmployee(request, id):
+    employee = Employee.objects.get(id=id)
+    employee.delete()
+    return redirect('allEmployee')  
 
-def addManagement(request):
-    
+def addManagement(request):    
     # return HttpResponse ("ok")
     return render (request, "base.html")
