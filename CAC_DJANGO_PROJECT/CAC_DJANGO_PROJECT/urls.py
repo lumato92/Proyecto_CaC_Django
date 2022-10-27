6293a6bcabafb0a78aca6a43507156304fc2bfb2
Eugenio <lugezz@gmail.com>
2022-10-27 09:36:57 -0300

    User Profile - Login

--- CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/urls.py
+++ CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/urls.py
@@ -1,13 +1,20 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('empleados/', include("employee.urls")),
+    path('login/', include('login.urls')),
     path('proveedores/', include('vendor.urls')),
+    path('users/', include('users.urls')),
     path('home/', views.home, name='home'),
-    path('auth0/',include('auth0.urls'))
+    path('auth0/', include('auth0.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- CAC_DJANGO_PROJECT/vendor/admin.py
+++ CAC_DJANGO_PROJECT/vendor/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Supplier, Category
 
-# Register your models here.
 
 admin.site.register(Supplier)
 admin.site.register(Category)
