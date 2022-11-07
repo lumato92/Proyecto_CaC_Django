ad1dbe296d537cff04772852f85303e860c3f02c
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-11-07 20:59:11 -0300

    correccion destinatario msj

--- CAC_DJANGO_PROJECT/auth0/models.py
+++ CAC_DJANGO_PROJECT/auth0/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+def full_name(self):
+    return f'{self.first_name} {self.last_name} ({self.username})'
+
+User.add_to_class("__str__", full_name)
