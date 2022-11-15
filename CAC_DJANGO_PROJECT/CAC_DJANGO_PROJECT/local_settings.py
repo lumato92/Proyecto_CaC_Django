15b4a2b4d8c36e45b91c9c15e5114d3cdcd0d0c5
Eugenio <lugezz@gmail.com>
2022-11-15 20:45:54 -0300

    Update git

---
+++ CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/local_settings.py
@@ -0,0 +1,11 @@
+
+MY_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pulenta_app',
+        'USER': 'artime',
+        'PASSWORD': 'artime80',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/settings.py
+++ CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-import os
+try:
+    from CAC_DJANGO_PROJECT.local_settings import MY_DATABASES
+except ImportError:
+    print('local_settings no encontrado')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +84,15 @@ WSGI_APPLICATION = 'CAC_DJANGO_PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if MY_DATABASES:
+    DATABASES = MY_DATABASES
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
@@ -129,7 +135,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
-
 
 
 MEDIA_URL = '/media/'
