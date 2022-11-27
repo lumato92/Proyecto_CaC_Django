008c51ce49449136b858d2a13609f21d2d27325a
lumato92 <lucasmtorrado@gmail.com>
2022-11-27 12:28:45 -0300

    comment local setting, minor fix en navbar

--- CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/local_settings.py
+++ CAC_DJANGO_PROJECT/CAC_DJANGO_PROJECT/local_settings.py
@@ -1,11 +1,11 @@
 
-MY_DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nombre_db',
-        'USER': 'my_user',
-        'PASSWORD': 'my_password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# MY_DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nombre_db',
+#         'USER': 'my_user',
+#         'PASSWORD': 'my_password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
